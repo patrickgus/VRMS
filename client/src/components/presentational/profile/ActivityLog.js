19d843850d86f3d0949e362f4b8da0307f94fcb2
Patrick Gustafson <53308172+patrickgus@users.noreply.github.com>
2020-08-03 15:46:56 -0700

    create ActivityLog component

---
+++ client/src/components/presentational/profile/ActivityLog.js
@@ -0,0 +1,25 @@
+import React from "react";
+import moment from "moment";
+
+const ActivityLogs = ({ context }) => {
+  const { logs } = context;
+  console.log(logs);
+
+  return (
+    <>
+      {logs
+        ? logs.map((log) => (
+            <div className="ActivityLog">
+              {" "}
+              <h3>{moment(log.date_logged).format("MM-DD-YYYY")}</h3>
+              <p>Category: {log.log_category}</p>
+              <p>Hours: {log.log_hours}</p>
+              <p>Notes: {log.log_notes}</p>
+            </div>
+          ))
+        : ""}
+    </>
+  );
+};
+
+export default ActivityLogs;
