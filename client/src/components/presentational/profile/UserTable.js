080b8a1868e5b499bbacd2472f159ccfea3bde97
Patrick Gustafson <53308172+patrickgus@users.noreply.github.com>
2020-09-14 08:33:51 -0700

    refactor to import user profile data from db

--- client/src/components/presentational/profile/UserTable.js
+++ client/src/components/presentational/profile/UserTable.js
@@ -1,41 +1,63 @@
-import React from 'react';
-import ProfileOption from '../profile/ProfileOption';
+import React from "react";
+import ProfileOption from "../profile/ProfileOption";
 
 const UserTable = ({ context }) => {
-    const { user, removeOption } = context;
+  const { user, removeOption } = context;
 
-    return (
-        <div className="user-info">
-            <table className="user-data">
-                <tbody>
-                    <tr>
-                        <th className="user-data__header">Name</th>
-                        <td className="user-data__info user-data">{user.name}</td>
-                    </tr>
-                    <tr>
-                        <th className="user-data__header">Email</th>
-                        <td className="user-data__info">{user.email}</td>
-                    </tr>
-                    <tr>
+  return (
+    <div className="user-info">
+      <table className="user-data">
+        <tbody>
+          <tr>
+            <th className="user-data__header">Name</th>
+            <td className="user-data__info user-data">
+              {user.name.firstName + " " + user.name.lastName}
+            </td>
+          </tr>
+          <tr>
+            <th className="user-data__header">Email</th>
+            <td className="user-data__info">{user.email}</td>
+          </tr>
+          {/* <tr>
                         <th className="user-data__header">Github</th>
                         <td className="user-data__info">{user.github}</td>
-                    </tr>     
+                    </tr>  */}
 
-                    {user.slack ?    
+          {user.currentRole ? (
+            <tr>
+              <th className="user-data__header">Current Role</th>
+              <td className="user-data__info user-data__info--flex">
+                <ProfileOption
+                  option={user.currentRole}
+                  removeOption={() => removeOption("currentRole", user.currentRole)}
+                />
+              </td>
+            </tr>
+          ) : (
+            ""
+          )}
+
+          {/* {user.slack ?    
                     (<tr>
                         <th className="user-data__header">Slack</th>
                         <td className="user-data__info">{user.slack}</td>
-                    </tr>):("")}
+                    </tr>):("")} */}
 
-                    {user.desiredRoles ? 
-                    (<tr>
-                        <th className="user-data__header">Desired Roles</th>
-                        <td className="user-data__info user-data__info--flex">
-                            {user.desiredRoles.map((option, index)=> (<ProfileOption key={index} option={option} removeOption={()=>removeOption("desiredRoles", option)}/>))}
-                        </td>
-                    </tr>):("")}
+          {user.desiredRole ? (
+            <tr>
+              <th className="user-data__header">Desired Role</th>
+              <td className="user-data__info user-data__info--flex">
+                <ProfileOption
+                  option={user.desiredRole}
+                  removeOption={() => removeOption("desiredRole", user.desiredRole)}
+                />
+              </td>
+            </tr>
+          ) : (
+            ""
+          )}
 
-                    {user.hackNights ? 
+          {/* {user.hackNights ? 
                         (<tr>
                             <th className="user-data__header">My Hack Nights</th>
                             <td className="user-data__info user-data__info--flex">
@@ -49,11 +71,11 @@ const UserTable = ({ context }) => {
                             <td className="user-data__info user-data__info--flex">
                                 {user.availability.map((option, index)=>(<ProfileOption key={index} option={option} removeOption={()=>removeOption("availability", option)}/>))}
                             </td>
-                        </tr>):("")}
-                </tbody>
-            </table>
-        </div>
-    )
-}
+                        </tr>):("")} */}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default UserTable;
