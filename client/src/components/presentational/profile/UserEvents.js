98d4631bfb8c2d4b82778ee5e31d4db11025e676
Patrick Gustafson <53308172+patrickgus@users.noreply.github.com>
2020-09-14 18:56:40 -0700

    delete console logs

--- client/src/components/presentational/profile/UserEvents.js
+++ client/src/components/presentational/profile/UserEvents.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React from "react";
 import moment from "moment";
 
 const UserEvents = ({ context }) => {
-    const { events } = context;
-    console.log("events", events);
+  const { events } = context;
 
-    return (
-        <div className="user-events">
-            <table className="user-data">
-                <tbody>
-                    <tr>
-                        <th className="user-data__header">Event</th>
-                        <th className="user-data__header">Date/Time</th>
-                        <th className="user-data__header">Link</th>
-                    </tr>
+  return (
+    <div className="user-events">
+      <table className="user-data">
+        <tbody>
+          <tr>
+            <th className="user-data__header">Event</th>
+            <th className="user-data__header">Date/Time</th>
+            <th className="user-data__header">Link</th>
+          </tr>
 
-                    {events ? events.map((event,index) => 
-                        (<tr key={index}>
-                            <td className="user-data__info">{event.name}</td>
-                            <td className="user-data__info">{moment(event.date).format("M/YY hA")}</td>
-                            <td className="user-data__info">{event.url}</td>
-                        </tr>)) 
-                        : ("")}
-                </tbody>
-            </table>
-        </div>
-    );
-}
+          {events
+            ? events.map((event, index) => (
+                <tr key={index}>
+                  <td className="user-data__info">{event.name}</td>
+                  <td className="user-data__info">
+                    {moment(event.date).format("M/YY hA")}
+                  </td>
+                  <td className="user-data__info">{event.url}</td>
+                </tr>
+              ))
+            : ""}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default UserEvents;
