11ed32fd0a8bccd30707e7fa18df661083e21eb2
Patrick Gustafson <53308172+patrickgus@users.noreply.github.com>
2020-09-21 09:13:29 -0700

    create getById routes and post route

--- backend/routers/timeTracker.router.js
+++ backend/routers/timeTracker.router.js
@@ -6,6 +6,8 @@ const TimeTracker = require("../models/timeTracker.model");
 // GET api/logs/
 router.get("/", (req, res) => {
   TimeTracker.find()
+    .populate("user")
+    .populate("project")
     .then((logs) => {
       res.json(logs);
     })
@@ -16,3 +18,48 @@ router.get("/", (req, res) => {
       });
     });
 });
+
+router.get("/user/:id", (req, res) => {
+  TimeTracker.find({ user: req.params.id })
+    .populate("user")
+    .populate("project")
+    .then((logs) => {
+      res.json(logs);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500).json({
+        message: `/GET Internal server error: ${err}`,
+      });
+    });
+});
+
+router.get("/project/:id", (req, res) => {
+  TimeTracker.find({ project: req.params.id })
+    .populate("user")
+    .populate("project")
+    .then((logs) => {
+      res.json(logs);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500).json({
+        message: `/GET Internal server error: ${err}`,
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  TimeTracker.create(req.body, function (err, log) {
+    if (err) {
+      console.log(err.errmsg);
+      res.json(err.errmsg);
+    } else {
+      const { id } = log;
+      console.log("Created with id: " + id);
+      res.status(201).json(id);
+    }
+  });
+})
+
+module.exports = router;
