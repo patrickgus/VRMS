dc250da92a831440a938c762d08c320d0191ffd1
Patrick Gustafson <53308172+patrickgus@users.noreply.github.com>
2020-10-15 16:02:37 -0700

    add function for calculating hours

--- client/src/components/presentational/profile/LogListItem.js
+++ client/src/components/presentational/profile/LogListItem.js
@@ -2,14 +2,18 @@ import React from "react";
 
 const LogListItem = (props) => {
   const log = props.log;
-  const { startDate, category, notes } = log;
+  const { startDate, endDate, category, notes } = log;
+  const minutes = 1000 * 60;
+  const hours = minutes * 60;
+  const timeDif = new Date(endDate).getTime() - new Date(startDate).getTime();
+  const hoursDif = Math.round(timeDif / hours);
   const startTime = new Date(startDate).toLocaleDateString("en-US");
-  
+
   return (
     <div className="LogListItem">
       <h3>{startTime}</h3>
       <p>Category: {category}</p>
-      <p>Hours: N/A</p>
+      <p>Hours: {hoursDif}</p>
       <p>Notes: {notes}</p>
     </div>
   );
