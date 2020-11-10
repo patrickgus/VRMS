1e05c303d462157b89587843be9fb03360c9c6c4
Patrick Gustafson <53308172+patrickgus@users.noreply.github.com>
2020-11-10 08:33:32 -0800

    create addLog form page

---
+++ client/src/pages/AddLogForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddLogForm = () => {
+  return (
+    <div className="AddLogForm">
+      <h3>Add a new Log</h3>
+    </div>
+  );
+};
+
+export default AddLogForm;
